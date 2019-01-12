d0d3f3cbfa652af837a2a278c6026671ea89e7bb
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-01-12 16:57:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mickey/abellanosapos/AddRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mickey.abellanosapos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipe)
+    }
+}
---
+++ app/src/main/java/com/example/mickey/abellanosapos/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mickey.abellanosapos
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        button_homeGoToAddRecipe.setOnClickListener {
+            startActivity(Intent(this, AddRecipeActivity::class.java))
+        }
+    }
+}
